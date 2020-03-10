8515eee41ecab3a8459c6f07feecc6564984014a
valentin lmr <valentinlmr28@gmail.com>
2020-03-10 16:18:18 +0100

    extarnal db

--- src/components/UI/Spiner/Spiner.js
+++ src/components/UI/Spiner/Spiner.js
@@ -2,7 +2,7 @@ import React from 'react';
 import style from './Spiner.module.css'
 const spiner = () => (
     <div className={style.FoldingCube}>
-    <div className={[style.Cube1, style.Cube].join (' ')}></div>
+    <div className={[style.Cube1, style.Cube].join (' ')}>Loading...</div>
     <div className={[style.Cube2, style.Cube].join (' ')}></div>
     <div className={[style.Cube4, style.Cube].join(' ')}></div>
     <div className={[style.Cube3, style.Cube].join(' ')}></div>
