7cbaf56984de42b0c4d331ab31e470c365370679
valentin lmr <valentinlmr28@gmail.com>
2020-03-13 09:34:05 +0100

    Give a style to the input

---
+++ src/components/UI/Input/Input.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import styles from './Input.module.css'
+const input = (props) => {
+
+    let inputElement = '';
+
+    switch (props.inputtype){
+        case('input'): inputElement = <input  className={styles.InputElement}{...props}/>; break
+        case('textarea'): inputElement = <textarea className={styles.InputElement} {...props}/> ; break
+        default: inputElement = <input className={styles.InputElement} {...props}/>
+    }
+
+    return (
+    <div className={styles.Input}>
+        <label className={styles.Label}>{props.lable}</label>
+        {inputElement}
+    </div>
+    )
+}
+export default input
