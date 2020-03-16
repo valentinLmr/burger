f847a4427e475103c9fd18ff2889f311e732fc8a
valentin lmr <valentinlmr28@gmail.com>
2020-03-16 11:05:08 +0100

    simple form done + enabled button if incorrect form

--- src/components/UI/Input/Input.js
+++ src/components/UI/Input/Input.js
@@ -3,17 +3,38 @@ import styles from './Input.module.css'
 const input = (props) => {
 
     let inputElement = '';
+    const inputStyles = [styles.InputElement]
 
-    switch (props.inputtype){
-        case('input'): inputElement = <input  className={styles.InputElement}{...props}/>; break
-        case('textarea'): inputElement = <textarea className={styles.InputElement} {...props}/> ; break
-        default: inputElement = <input className={styles.InputElement} {...props}/>
+    if(props.invalid && props.shouldValidate && props.touched ) {
+        inputStyles.push(styles.Invalid)
+    }
+
+    switch (props.elementType){
+        case('input'): inputElement = <input  onChange={props.changed} className={inputStyles.join(' ')}{...props.elementConfig} value={props.value} />; break
+        case('textarea'): inputElement = <textarea onChange={props.changed} className={styles.InputElement} {...props.elementConfig} value={props.value} /> ; break
+        case('select'): inputElement = (
+        <select
+            className={styles.InputElement}
+            value={props.value}
+            onChange={props.changed}>
+            {props.elementConfig.options.map(option => (
+                <option value={option.value}>
+                    {option.displayValue}
+                </option> 
+            ))}
+        </select>); break
+        default: inputElement = <input onChange={props.changed} className={styles.InputElement} {...props.elementConfig}  />
+    }
+
+    let validationError = null;
+    if (props.invalid && props.touched) {
+    validationError = <p><i>Please enter a valid {props.type}! </i></p>;
     }
 
     return (
     <div className={styles.Input}>
         <label className={styles.Label}>{props.lable}</label>
-        {inputElement}
+        {inputElement}{validationError}
     </div>
     )
 }
